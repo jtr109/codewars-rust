cc81800e1225f1ce8ba34ad542abc8dcca715118
Ryan Li <conbas2019@gmail.com>
2021-05-16 21:07:10 +0800

    feat: resolved

--- int32_to_ipv4/src/lib.rs
+++ int32_to_ipv4/src/lib.rs
@@ -1,7 +1,31 @@
+/*!
+ * https://www.codewars.com/kata/52e88b39ffb6ac53a400022e/train/rust
+ */
+
+pub fn int32_to_ip(int: u32) -> String {
+    let base = 2_u32.pow(8);
+    let mut digits = vec![];
+    let mut num = int;
+    while digits.len() != 4 {
+        digits.push(num % base);
+        num /= base;
+    }
+    digits
+        .iter()
+        .rev()
+        .map(|d| d.to_string())
+        .collect::<Vec<String>>()
+        .join(".")
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn basic() {
+        assert_eq!(int32_to_ip(2154959208), "128.114.17.104");
+        assert_eq!(int32_to_ip(2149583361), "128.32.10.1");
+        assert_eq!(int32_to_ip(0), "0.0.0.0");
     }
 }
