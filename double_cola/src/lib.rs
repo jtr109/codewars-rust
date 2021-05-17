b8b986221f0a2e8861054a5a51f76760c693b489
Ryan Li <conbas2019@gmail.com>
2021-05-17 13:38:22 +0800

    feat: add the problem

--- double_cola/src/lib.rs
+++ double_cola/src/lib.rs
@@ -1,7 +1,38 @@
+/// _BigBang_ gang
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+enum Name {
+    Sheldon,
+    Leonard,
+    Penny,
+    Rajesh,
+    Howard,
+}
+
+/// Just to make code look a bit nicer
+type Names = Vec<Name>;
+
+/// Will return the `Name` of the person who will drink the `n`-th cola.
+fn who_is_next(names: &Names, n: usize) -> Name {
+    todo!("Your code goes here!")
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_basic() {
+        let names = &vec![
+            Name::Sheldon,
+            Name::Leonard,
+            Name::Penny,
+            Name::Rajesh,
+            Name::Howard,
+        ];
+
+        assert_eq!(who_is_next(names, 1), Name::Sheldon);
+        assert_eq!(who_is_next(names, 6), Name::Sheldon);
+        assert_eq!(who_is_next(names, 52), Name::Penny);
+        assert_eq!(who_is_next(names, 7_230_702_951), Name::Leonard);
     }
 }
