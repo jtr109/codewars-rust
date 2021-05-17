9baa03896b0a3cc944b3180fbe9c3ecce664a909
Ryan Li <conbas2019@gmail.com>
2021-05-17 14:24:47 +0800

    feat: resolved

--- double_cola/src/lib.rs
+++ double_cola/src/lib.rs
@@ -12,14 +12,13 @@ enum Name {
 type Names = Vec<Name>;
 
 /// Will return the `Name` of the person who will drink the `n`-th cola.
-fn who_is_next(names: &Names, mut n: usize) -> Name {
-    let mut double = 1;
-    while 5 * double < n {
-        n -= 5 * double;
-        double *= 2;
+fn who_is_next(names: &Names, n: usize) -> Name {
+    let len = names.len();
+    let mut r = n - 1;
+    while r >= len {
+        r = (r - len) / 2;
     }
-    let index = (n - 1) / double;
-    *names.iter().nth(index).unwrap()
+    *names.iter().nth(r).unwrap()
 }
 
 #[cfg(test)]
@@ -37,11 +36,12 @@ mod tests {
         ];
 
         assert_eq!(who_is_next(names, 1), Name::Sheldon);
+        assert_eq!(who_is_next(names, 5), Name::Howard);
         assert_eq!(who_is_next(names, 6), Name::Sheldon);
-        assert_eq!(who_is_next(names, 7), Name::Sheldon);
-        assert_eq!(who_is_next(names, 8), Name::Leonard);
-        assert_eq!(who_is_next(names, 30), Name::Howard);
-        assert_eq!(who_is_next(names, 31), Name::Sheldon);
+        assert_eq!(who_is_next(names, 15), Name::Howard);
+        assert_eq!(who_is_next(names, 16), Name::Sheldon);
+        assert_eq!(who_is_next(names, 35), Name::Howard);
+        assert_eq!(who_is_next(names, 36), Name::Sheldon);
         assert_eq!(who_is_next(names, 52), Name::Penny);
         assert_eq!(who_is_next(names, 7_230_702_951), Name::Leonard);
     }
